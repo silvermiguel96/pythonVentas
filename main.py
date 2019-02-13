a76306d319c5378eae2a11aeead4b7a09c9b515c
Miguelangel <silvermiguel96@gmail.com>
2019-02-12 21:31:14 -0500

    Using function in our project

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 
-clients = 'pablo,ricardo'
+clients = 'pablo,ricardo,'
+
+def crear_cliente(client_name):
+    global clients
+    clients += client_name
+    _add_coma()
+
+def list_clients():
+    global clients
+    print(clients)
+
+def _add_coma():
+    global clients
+    clients += ','
 
 if __name__ == '__main__':
-  clients += ',david'
-  print(clients)
+  
+  list_clients()
+  crear_cliente('david')
+  list_clients()
