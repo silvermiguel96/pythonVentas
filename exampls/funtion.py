4bd8af145cf36dab150f95e8f423127435f74c91
Miguelangel <silvermiguel96@gmail.com>
2019-02-12 21:06:30 -0500

    function

---
+++ exampls/funtion.py
@@ -0,0 +1,12 @@
+type(1) 
+un_entero = int('5')
+print(un_entero)
+type(un_entero)
+a = bool('a')
+print(a)
+def suma_de_dos_numeros(x, y):
+    return x + y
+print('Hola',suma_de_dos_numeros(10, 15))
+suma = suma_de_dos_numeros(5, 15)
+print(suma)
+type(suma_de_dos_numeros(5, 15))
