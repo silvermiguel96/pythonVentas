8304c4a8c5a0cd450665dd05785ea16172d8eadd
Miguelangel <silvermiguel96@gmail.com>
2019-02-13 22:25:21 -0500

    Conditional Estrunturing

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ clients = 'pablo,ricardo,'
 
 def crear_cliente(client_name):
     global clients
-    clients += client_name
-    _add_coma()
+    
+    if client_name not in clients:
+        clients += client_name
+        _add_coma()
+    else:
+      print('Client alread is in the client\'s list')
 
 def list_clients():
     global clients
@@ -14,8 +18,23 @@ def _add_coma():
     global clients
     clients += ','
 
+def _print_welcome():
+  print('Welcome to platzi Shop')
+  print('*' * 50)
+  print('What would you like to do today?')
+  print('[C]reate client')
+  print('[D]elete client')
+
 if __name__ == '__main__':
-  
-  list_clients()
-  crear_cliente('david')
-  list_clients()
+    _print_welcome()
+
+    command = input()
+    
+    if command == 'C':
+        client_name = input('What is the client name ? ')
+        crear_cliente(client_name)
+        list_clients()
+    elif command == 'D':
+      pass
+    else:
+      print('Invalid command')
