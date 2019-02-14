f85d8b934cbc832566d7f8b4dc2278fe6239f812
Miguelangel <silvermiguel96@gmail.com>
2019-02-13 22:46:12 -0500

    String in python

---
+++ exampls/string.py
@@ -0,0 +1,12 @@
+country = 'Colombia'
+country[1] #o 
+country[-1] #a
+country[-2] #i
+len(country) # 8
+
+second_letter = country[1]
+print(second_letter)
+id(second_letter) # Donde esta en nuestra memoria   4461106712
+other_var = '0'
+id(other_var) # Su espacio es en 4461106712
+id('i') # 4461106714
