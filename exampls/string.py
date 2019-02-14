6cc10f977c7039529ab6b88a7cc80399153254fa
Miguelangel <silvermiguel96@gmail.com>
2019-02-13 23:01:23 -0500

    operations with string in python

--- exampls/string.py
+++ exampls/string.py
@@ -10,3 +10,18 @@ id(second_letter) # Donde esta en nuestra memoria   4461106712
 other_var = '0'
 id(other_var) # Su espacio es en 4461106712
 id('i') # 4461106714
+
+
+country = 'Mexico' #4461106777
+country += 's'
+id(country) # 4461106788
+
+# Operations with string  in python 
+
+platzi = 'platzi'
+platzi.upper() # PLATZI
+platzi.startswith('p') #true
+platzi.startswith('l') # false
+dir(platzi)
+
+
