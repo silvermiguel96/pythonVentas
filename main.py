c2c34907e93d6e65f6339c32965bf7b48915f6ac
Miguelangel <silvermiguel96@gmail.com>
2019-02-14 12:25:12 -0500

    Operations strings and comand update

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def list_clients():
     global clients
     print(clients)
 
+def update_client(client_name, updated_client_name):
+    global clients
+    
+    if client_name in clients:
+      clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+      print('Client is not in clients list')
+
 def _add_coma():
     global clients
     clients += ','
@@ -23,18 +31,28 @@ def _print_welcome():
   print('*' * 50)
   print('What would you like to do today?')
   print('[C]reate client')
+  print('[U]pdate client')
   print('[D]elete client')
+
+def _get_client_name():
+    return input('What is the client name ? ')
 
 if __name__ == '__main__':
     _print_welcome()
 
     command = input()
+    command = command.upper()
     
     if command == 'C':
-        client_name = input('What is the client name ? ')
+        client_name = _get_client_name()
         crear_cliente(client_name)
         list_clients()
     elif command == 'D':
       pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_client_name = input('What is the updated client name ? ')
+        update_client(client_name, updated_client_name) 
+        list_clients()
     else:
       print('Invalid command')
